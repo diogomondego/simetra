e7b351362d609f3a4c6cb293070d64d001087469
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2021-06-24 17:38:28 -0300

    implementing pagination in routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -7,15 +7,22 @@ const router = express.Router()
 const db = require("./database/db.js")
 
 router.get('/posts', (req, res) => {
+  const { limit = 6, page = 1 } = req.query
+  const offset = limit * (page - 1)
+
   db.all(
     `
     SELECT id, title, description, image, published_at 
     FROM posts
+    ORDER BY published_at DESC
+    LIMIT ${limit}
+    OFFSET ${offset}
     `,
     function (err, rows) {
       if (err) {
         return console.log(err)
       }
+
       return res.json(rows)
     }
   )
