5a0cd84912cfed0f96739b01110013317016ca4b
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2021-07-03 13:48:03 -0300

    implementing PostList and adjusting css extension

--- web/src/Components/Header/index.js
+++ web/src/Components/Header/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import './styles.css'
+import styles from './styles.module.css'
 
 import logo from '../../assets/header.svg'
 
 export function Header() {
   return (
-    <header>
-      <nav className='container'>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
         <a href="/">
-          <img src={logo} className='logo' />
+          <img src={logo} alt='Simetra' className={styles.logo} />
         </a>
       </nav>
     </header>
--- web/src/Pages/Home/index.js
+++ web/src/Pages/Home/index.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import './styles.css'
+import React, { useEffect, useState } from 'react'
+import styles from './styles.module.css'
+
 import { Header } from '../../Components/Header'
+import { PostsList } from '../../Components/PostsList'
+import api from '../../services/api'
 
 export function Home() {
+  const [posts, setPosts] = useState([])
+  const [page, setPage] = useState(2)
+
+  async function fetchPosts() {
+    const { data } = await api.get(`/posts/?limit=6&page=${page}`)
+    setPosts(data)
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <div>
       <Header />
-      <h1>Blog</h1>
+      <main>
+        <div>
+          <div className={styles.header}>
+            <h1>Blog</h1>
+            <div className={styles.rectangle} />
+          </div>
+          <section>
+            <PostsList posts={posts} />
+          </section>
+        </div>
+      </main>
     </div>
   )
 }
