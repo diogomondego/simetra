cafdc61daa579e5b808785586ccf265d89a1848b
Diogo Mondego dos Santos <dm_dossantos@hotmail.com>
2021-07-08 10:32:59 -0300

    Adjusting icon and PostSkeleton

--- mobile/src/components/PostsSkeleton.js
+++ mobile/src/components/PostsSkeleton.js
@@ -7,30 +7,7 @@ import colors from '../styles/colors';
 export function PostsSkeleton() {
   return (
     <SkeletonPlaceholder>
-      <View style={{
-        flexDirection: 'row',
-        marginVertical: 20,
-        borderRadius: 7.5,
-
-        // Para funcionar o box shadow necessita de:
-        backgroundColor: colors.white_background,
-
-        // Box shadow iOS
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 3,
-        },
-        shadowOpacity: 0.27,
-        shadowRadius: 4.65,
-
-        // Box shadow Android
-        elevation: 6,
-        // marginHorizontal: 30,
-        width: '100%',
-        height: 120,
-      }}>
-      </View>
+      <View style={styles.container} />
     </SkeletonPlaceholder>
   )
 }
